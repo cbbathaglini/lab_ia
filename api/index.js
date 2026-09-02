852d822baf8fbf2c6570b89ac35859a7d6d2082d
Carine Bertagnolli Bathaglini <cbbathaglini@gmail.com>
2026-09-01 21:22:14 -0300

    Adapt Vercel function request handling

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import server from "../dist/server/server.js";
 
-export default async function handler(request) {
-  return server.fetch(request);
+export default async function handler(req, res) {
+  const host = req.headers.host ?? "localhost";
+  const protocol = req.headers["x-forwarded-proto"] ?? "https";
+  const request = new Request(`${protocol}://${host}${req.url}`, {
+    method: req.method,
+    headers: req.headers,
+    body: ["GET", "HEAD"].includes(req.method) ? undefined : req,
+    duplex: "half",
+  });
+
+  const response = await server.fetch(request);
+  res.statusCode = response.status;
+  response.headers.forEach((value, key) => res.setHeader(key, value));
+  res.end(Buffer.from(await response.arrayBuffer()));
 }
